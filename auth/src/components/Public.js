06ebcac38272ad7e2e1126d5cdbab35e27b2c289
Farid Hamida <faridhamida22@gmail.com>
2019-12-18 12:11:23 -0800

    updates

---
+++ auth/src/components/Public.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Typography } from "@material-ui/core";
+
+export default function Public() {
+  return (
+    <div>
+      <Typography variant="h3" color="primary">
+      Hi!
+      </Typography>
+    </div>
+  );
+}
